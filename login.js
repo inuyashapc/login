ebb00b2abcb137980237734b6b92dc69b708f300
AnhND <anhndhe161107@fpt.edu.vn>
2023-04-12 12:54:51 +0700

    Huyen ngao cho so 1 vn

---
+++ login.js
@@ -0,0 +1,10 @@
+var email = document.querySelector("#email");
+var password = document.querySelector("#password");
+
+function checkEmail(input) {
+  if (input == "") {
+    alert("Please enter a valid email");
+  } else {
+    console.log(input);
+  }
+}
